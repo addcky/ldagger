bcf9f88eaeff983e55eed43716c0c6efd01c08f6
addcky <addcky@aliyun.com>
2016-08-18 12:10:10 +0800

    app打包初长成

--- ctrl/controllers.js
+++ ctrl/controllers.js
@@ -13,9 +13,6 @@ angular.module('lifetools.controllers', [])
 			scope.enterapp = function(){
 				scope.firstSvgShow = false;
 			}
-			
-			//console.log(scope.firstSvgShow)
-			
 		}
 	}
 }])
@@ -46,48 +43,90 @@ angular.module('lifetools.controllers', [])
 	
 	
 	var cook = $scope.cook ={};
-	cook.space = true;
-	cook.noSpace = false;
-	
-	/*cook.getCookMs = function(){
-		if(cook.name != '' && cook.name != ' '){
-			$http.get('view/cookname.php',{
-				params:{
-					name:cook.name
-				}
-			}).success(function(res){
-				if(res.reason === 'Success'){
-					cook.res = res.result.data
-					console.log(cook.res.id)
+	cook.noData = '';
+	cook.slideChanged = cook.pagerClick = function(idx){
+		
+		if(idx == 1){
+			cook.token = localStorage.getItem('token');
+			if(cook.token!== null && cook.token !== ''){
+				cook.space = false;
+				cook.data = JSON.parse(localStorage.getItem('user-'+cook.token)).data;
+				if(cook.data){
+					cook.noData = '';
+					//console.log(cook.data);
+				}else{
+					cook.noData = '厨房空空如也。';
 				}
 				
-			})
-		}		
+			}else{
+				cook.space = true;
+			}
+		}
 	}
-	cook.getCookListMs = function(listName){		
-		$http.get('view/cooklistname.php',{
-			params:{
-				listname:listName
-			}
-		}).success(function(res){
-			if(res.reason === 'Success'){
-				cook.res = res.result.data
-				//console.log(res.result.data)
-			}
-			
-		})
-	}*/
+	cook.getCookMs = function(){
+ 		if(cook.name != '' && cook.name != ' '){
+ 			$http.get('view/cookname.php',{
+ 				params:{
+ 					name:cook.name
+ 				}
+ 			}).success(function(res){
+ 				if(res.reason === 'Success'){
+ 					cook.res = res.result.data
+ 				}
+ 				
+ 			})
+ 		}		
+ 	}
+ 	cook.getCookListMs = function(listName){		
+ 		$http.get('view/cooklistname.php',{
+  			params:{ 							
+ 				listname:listName
+  			}
+  		}).success(function(res){
+ 			if(res.reason === 'Success'){
+ 				cook.res = res.result.data
+ 				//console.log(res.result.data)
+  			}			
+ 		})
+	}
+	
 	cook.setCookMs = function(that){
 		cook.token = localStorage.getItem('token');
-		if(cook.token){
-			cook.oldValue = localStorage.getItem('token'+cook.token);
-			
-			cook.cookieName = 'token-'+cook.token;
-			cook.cookieValue = JSON.stringify(that);
-			localStorage.setItem(cook.cookieName,cook.cookieValue);
+		
+		if(cook.token!== null && cook.token !== ''){
+			cook.oldValue = JSON.parse(localStorage.getItem('user-'+cook.token));
+			//console.log(cook.oldValue);
+			cook.newObj = {};
+			cook.newObj.username = cook.oldValue.username;
+			cook.newObj.psw = cook.oldValue.psw;
+			if(!cook.oldValue.data){
+				cook.newObj.data = [that];
+			}else{
+				cook.oldValue.data.unshift(that);
+				cook.newObj.data = cook.oldValue.data;
+			}
+			localStorage.setItem(('user-'+cook.token),JSON.stringify(cook.newObj));
 			$rootScope.showAlert('','已成功保存到"我的私房菜"');
+		}else{
+			$rootScope.showAlert('','请先登录');
 		}
 		
+	}
+	cook.dele = function(that){
+		cook.oldValue = JSON.parse(localStorage.getItem('user-'+cook.token));
+		//console.log(cook.oldValue);
+		cook.newObj = {};
+		cook.newObj.username = cook.oldValue.username;
+		cook.newObj.psw = cook.oldValue.psw;
+		cook.newObj.data = [];
+		for(var i=0; i<cook.oldValue.data.length;i++){
+			if(cook.oldValue.data[i].title != that.title){
+				cook.newObj.data.push(cook.oldValue.data[i])
+			}
+		}
+		
+		cook.data = cook.newObj.data;
+		localStorage.setItem(('user-'+cook.token),JSON.stringify(cook.newObj));
 	}
 	cook.enterKeyup = function(e){
         var keycode = window.event?e.keyCode:e.which;
@@ -98,7 +137,266 @@ angular.module('lifetools.controllers', [])
 		
 })
 .controller('infoController', function($rootScope,$scope, $http, $ionicLoading,$ionicPopup) {
+	//--------------------------info--------------------------------
 	
+	//获取当前日期
+	$scope.getdate = function(){
+		$scope.dates = new Date();
+		$scope.year = $scope.dates.getFullYear();
+		$scope.month = $scope.dates.getMonth()+1;
+		$scope.date = $scope.dates.getDate();
+	}
+	$scope.getdate();
+	
+	//有请求数据加载时遮罩层显示和隐藏
+	$scope.show = function() {
+		$ionicLoading.show({
+			template: '<i class="icon ion-ios-infinite loadlogo"></i>',//'Loading...'
+			noBackdrop:true
+		});
+	};
+	$scope.hide = function() {
+		$ionicLoading.hide();
+	};
+	
+	
+	
+	/*
+	 
+	 * 今日天气
+	 * 
+	 * */
+	//今日天气变量的集合对象
+	
+	var wth = $scope.wth = {};
+	wth.tag = true;
+	//今日历史变量的集合对象
+	var datas = $scope.datas = {};
+	datas.tag = true;
+	//今日助理变量的集合对象
+	var siri = $scope.siri = {};
+	siri.tag = true;
+	//请求今天日历数据
+	wth.calenShow = false;
+	wth.getDateMs = function(){
+		$http.get('view/calendar.php',{
+			params:{
+				year: $scope.year,
+				month: $scope.month,
+				date: $scope.date
+			}
+		}).success(function(res){
+			
+			if(res.reason === 'Success' || res.reason === 'success'){
+				wth.resp = res.result.data;
+				wth.arrYMD = wth.resp.date.split('-');
+				//console.log(wth.resp);
+			}else{
+				$rootScope.alertCheck();
+			}
+		}).error(function(){
+			$rootScope.alertRefresh();
+		});
+	}
+	wth.getDateMs();
+	wth.calenClick = function(){
+		wth.calenShow?wth.calenShow = false:wth.calenShow = true;
+		
+	}
+	
+	wth.city = '广州';
+	wth.getWthMs = function(){
+		$http.get('view/weather.php',{
+			params:{
+				city: wth.city				
+			}
+		}).success(function(res){
+			//console.log(res);
+			if(res.reason === 'successed!'){
+				//console.log(res.result.data);
+				$rootScope.wthMore = wth.res = res.result.data;
+				//wth.res = res.result.data;
+				//wth.arrYMD = wth.res.date.split('-');
+				
+			}else if(res.reason === '暂不支持该城市'){
+				$rootScope.alertNoCity();
+			}else{
+				$rootScope.alertCheck();
+			}
+		}).error(function(){
+			$rootScope.alertRefresh();
+		});
+	}
+	wth.getWthMs();
+	wth.enterKeyup = function(e){
+        var keycode = window.event?e.keyCode:e.which;
+        if(keycode==13 && wth.city != ''){
+            //console.log('2');
+            wth.getWthMs();
+        }
+    };
+    wth.doRefresh = function(){
+    	wth.getDateMs();
+		wth.getWthMs();
+		$scope.$broadcast('scroll.refreshComplete');
+	}
+	
+	
+	
+	/*
+	 
+	 * 今日历史
+	 * 
+	 * */
+	
+	//今日历史“查看更多”提示区域默认隐藏
+	datas.lookMore = false;
+	//今日历史搜索框默认文本值
+	datas.txtdate = '';
+	
+	//今日历史发送请求，获取数据
+	$scope.getMs = function(isLoadingShow) {
+		if(!isLoadingShow){
+			$scope.show();
+			//console.log('2')
+		}
+		$http.get('view/history.php', {
+			params: {
+				month: $scope.month,
+				date: $scope.date
+			}
+		}).success(function(res) {
+			//console.log(res);
+			if(res.reason === 'success'){
+				$scope.num = 0;
+				$scope.rest = [];
+				$scope.hitem = [];
+				angular.forEach(res.result, function(data, idx, arr) {
+					$scope.obj = {};
+					$scope.obj.title = data.title;
+					$scope.obj.e_id = data.e_id;
+					$scope.obj.data = data.date.split("年")[0];
+					$scope.rest.unshift($scope.obj);
+				});
+				$scope.hide();
+				for(var i = 0; i < 10; i++) {
+					$scope.hitem.push($scope.rest[i]);
+				}
+				datas.lookMore = true;
+				datas.moretxt = '上拉查看更多';
+				datas.stopMore = true;
+				datas.loadMore = function() {
+					//console.log('s:'+$scope.rest.length);
+					$scope.hitem = [];
+					$scope.num += 10;
+					if($scope.num < $scope.rest.length) {
+						for(var i = 0; i < $scope.num; i++) {
+							$scope.hitem.push($scope.rest[i]);
+						}
+						//console.log($scope.hitem);
+					} else {
+						$scope.hitem = $scope.rest;
+						datas.stopMore = false;
+						datas.moretxt = '没有更多了...'
+					}
+					$scope.$broadcast('scroll.infiniteScrollComplete');
+				};				
+			}else{
+				$rootScope.alertCheck();
+			}
+		}).error(function(){
+			$rootScope.alertRefresh();
+		});
+	}
+		//----默认加载内容
+	$scope.getMs();
+	//重置为当前日期
+	datas.gotoday = function(){
+		$scope.getdate();
+		$scope.getMs();
+	}
+	//下拉刷新
+	datas.doRefresh = function(){
+		$scope.getMs(true);
+		$scope.$broadcast('scroll.refreshComplete');
+	}
+	//回车获取搜索框文本，发送请求
+	datas.myKeyup = function(e){
+        var keycode = window.event?e.keyCode:e.which;
+        if(keycode==13){
+            //console.log('2');
+            $scope.dealtxt = /^((([13578]|(1[02]))(\-|\/|\.)([1-9]|([12]\d)|(3[01])))|(([469]|11)(\-|\/|\.)([1-9]|([12]\d)|30))|(2(\-|\/|\.)([1-9]|([12]\d))))$/
+			if(datas.txtdate != '') {
+				if(!$scope.dealtxt.test(datas.txtdate)){
+					$rootScope.showAlert('日期输入错误','参考格式 为三种(月/日)，例如：6/18 or 6-18 or 6.18');				
+				}else{				
+					$scope.month = datas.txtdate.split(/\-|\/|\./)[0];
+					$scope.date = datas.txtdate.split(/\-|\/|\./)[1];
+					//console.log($scope.month);
+					$scope.getMs();
+				}
+			}
+        }
+    };
+    /*
+	 
+	 * 生活小助理
+	 * 
+	 * */
+    siri.getJokerMs = function(joker,opt){
+    	$http.get('view/joker.php',{
+    		params:{
+    			joker:joker
+    		}
+    	}).success(function(res){
+    		if(res.reason === 'success'){
+    			if (opt === 'joker'){
+    				siri.joker = res.result[0].content;
+    				
+    			}else{
+    				siri.img = res.result[0];
+    				//console.log(siri.img);
+    			}
+    		}else{
+				$rootScope.alertCheck();
+			}
+    	}).error(function(){
+			$rootScope.alertRefresh();
+		});   	
+    }
+    siri.getJokerMs('','joker');//getJoker
+    siri.getJokerMs('pic','pic');//getPic
+    //console.log(siri.joker[0].content);
+    siri.getPhoneMs = function(){
+    	$http.get('view/phone.php',{
+    		params:{
+    			phone:siri.phone
+    		}
+    	}).success(function(res){
+    		if(res.reason === 'Return Successd!'){
+    			siri.res = ['地区：'+res.result.province+res.result.city,
+    			            '类型：'+res.result.company+res.result.card];
+    		}else{
+    			siri.res = [res.reason,''];
+			}
+    	}).error(function(){
+			$rootScope.alertRefresh();
+		});   	
+    }
+    siri.enterKeyup = function(e){
+        var keycode = window.event?e.keyCode:e.which;
+        if(keycode==13 && siri.phone != ''){
+            //console.log('2');
+        	siri.getPhoneMs();
+        }
+    };
+    siri.doRefresh = function(){
+    	siri.getJokerMs('','joker');//getJoker
+        siri.getJokerMs('pic','pic');
+		$scope.$broadcast('scroll.refreshComplete');
+	}
+    
+    
 	})
 .controller('cookcontController', function($stateParams,$rootScope,$scope, $http, $ionicLoading,$ionicPopup) {
 	var cook = $scope.cook = {};
@@ -116,12 +414,6 @@ angular.module('lifetools.controllers', [])
 		});
 	}
 	cook.getCookMs();
-	cook.setCookMs = function(that){
-		cook.cookieName = 'cook-'+that.id;
-		cook.cookieValue = JSON.stringify(that);
-		localStorage.setItem(cook.cookieName,cook.cookieValue);
-		$rootScope.showAlert('','已成功保存到"我的私房菜"');
-	}
 	cook.doRefresh = function(){
 		cook.getCookMs();
 		$scope.$broadcast('scroll.refreshComplete');
